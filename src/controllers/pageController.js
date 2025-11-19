7addd1e26d35eb99d35aab7a41c4e88955c4f2a7
RiccardoAccatino <20054233@studenti.uniupo.it>
2025-11-19 14:57:26 +0100

    Inizio Homepage, Continuare nav

--- src/controllers/pageController.js
+++ src/controllers/pageController.js
@@ -1,9 +1,10 @@
 const pageController = {
     getHome: (req, res) => {
-        // Cerca il file in src/views/public/home.ejs
-        res.render('public/home', { title: 'Home - Officina Fitness' });
+        res.render('public/home', { 
+            title: 'Home - Officina Fitness',
+            isHome: true // <--- AGGIUNGI QUESTA RIGA
+        });
     },
-
     getServices: (req, res) => {
         res.render('public/services', { title: 'I Nostri Corsi' });
     }
